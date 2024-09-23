7d2c2a084fd1a9cb400d2c84201ab2a6e9428433
KatheRojasO <kathe.rojaso@gmail.com>
2024-09-23 13:18:02 +0200

    Override toString method in record to change the way it shows info

--- src/main/java/taskManagement/Task.java
+++ src/main/java/taskManagement/Task.java
@@ -6,4 +6,9 @@ public record Task(
         String taskName,
         LocalDateTime deadline,
         int priority) {
+
+    @Override
+    public String toString() {
+        return String.format("Task Name: %s\nDeadline: %s\nPriority: %d\n", taskName, deadline, priority);
+    }
 }
